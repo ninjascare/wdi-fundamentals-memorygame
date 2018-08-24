9d269371dc1eaa29feac77f9fccaaf09e2b9c3ed
Kevin Rosales <kevinrosales@Kevins-MacBook-Air.local>
2018-08-23 21:38:24 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,17 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
+var checkForMatch = function(){
 if (cardsInPlay[0] === cardsInPlay[1]){
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.")
 }
+};
+var flipCard = function(cardId){
+if (cardsInPlay.length === 2) {
+checkForMatch();
 } 
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped" + '' + cards[cardId]);
+};
+flipCard(0);
